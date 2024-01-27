1468d5bae24d0ffecf153f7924869b10866dd4c1
Pascal Benimana <pascal.benimana6@gmail.com>
2024-01-27 17:32:21 -0500

    compiled candidate.java

--- Candidate.java
+++ Candidate.java
@@ -1,5 +1,9 @@
 class Candidate{
 	public static void main(String args[]){
+		int a = 12;
+		int b = 3;
+		int sum = a * b;
+		System.out.println(a +" * "+b+" = "+sum);
 		System.out.println("Welcome here!");
 	}
 
